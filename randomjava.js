e0d3a94a6fdedb2db31c3447f661662f1c434381
AdwaithReborn <adwaithashokan17@gmail.com>
2026-09-21 23:46:26 +0530

    Update randomjava.js

--- randomjava.js
+++ randomjava.js
@@ -49,3 +49,21 @@ public class Main {
         System.out.println("Largest: " + largest);
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "Adwaith Asokan";
+        int count = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            char ch = Character.toLowerCase(text.charAt(i));
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+
+        System.out.println("Vowels: " + count);
+    }
+}
