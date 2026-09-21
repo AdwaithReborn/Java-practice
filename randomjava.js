c8269e7a57d1d8154501430881789535b4c354c2
AdwaithReborn <adwaithashokan17@gmail.com>
2026-09-21 23:46:42 +0530

    Add Fibonacci sequence implementation in Main class

--- randomjava.js
+++ randomjava.js
@@ -67,3 +67,19 @@ public class Main {
         System.out.println("Vowels: " + count);
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
