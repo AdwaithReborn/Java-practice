7719b6124a35c3a30a0d053bb048bc29f7aa3f98
AdwaithReborn <adwaithashokan17@gmail.com>
2026-09-21 23:45:53 +0530

    Add prime number check to Main class

--- randomjava.js
+++ randomjava.js
@@ -13,3 +13,23 @@ public class Main {
         System.out.println(reverse);
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean prime = true;
+
+        if (num <= 1) {
+            prime = false;
+        }
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        System.out.println(prime ? "Prime" : "Not Prime");
+    }
+}
