1ad321440f3063f67675af6daac30cc7c2e4263a
AdwaithReborn <adwaithashokan17@gmail.com>
2026-09-21 23:46:10 +0530

    Add Main class to find the largest number in array

--- randomjava.js
+++ randomjava.js
@@ -33,3 +33,19 @@ public class Main {
         System.out.println(prime ? "Prime" : "Not Prime");
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {12, 45, 7, 89, 34};
+
+        int largest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > largest) {
+                largest = numbers[i];
+            }
+        }
+
+        System.out.println("Largest: " + largest);
+    }
+}
