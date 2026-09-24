67911759d82b419a4744bd984289075fb598c7c8
Adwaith Ashokan <adwaithashokan17@gmail.com>
2026-09-24 14:43:19 +0530

    Update randomjava.js

--- randomjava.js
+++ randomjava.js
@@ -168,3 +168,17 @@ public class Main {
         }
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 20, 40, 10};
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j]) {
+                    System.out.println("Duplicate: " + numbers[i]);
+                }
+            }
+        }
+    }
+}
